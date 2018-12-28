20f1d9ffaad17f9b907ebeed7c6ee5751776ff09
online6731 <mohammad.parsian@gmail.com>
2018-12-28 21:46:37 +0330

    Gitacora saved 3 changed files

--- template_engine/TemplateEngine.py
+++ template_engine/TemplateEngine.py
@@ -1170,8 +1170,7 @@ def test_templates(templates, try_count=5, rounds_count=1, save_result=True):
 
 			#pprint(question, indent=4)
 
-			if not problems:
-				logger.info(f'SUCCESSFULL')
+			if not problem: logger.info(f'SUCCESSFULL')
 
 				if CONFIG.use_mongo: add_question_to_mongo(question)
 
