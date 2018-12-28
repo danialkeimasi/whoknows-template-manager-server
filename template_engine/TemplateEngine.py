543d7da0754ecd82501a027ebacbefa4427e1ba1
online6731 <mohammad.parsian@gmail.com>
2018-12-28 05:28:04 +0330

    Gitacora saved 1 changed files

--- template_engine/TemplateEngine.py
+++ template_engine/TemplateEngine.py
@@ -1222,7 +1222,7 @@ datasets = ['movie', 'director', 'song', 'actor', 'footballPlayer', 'footballTea
 			'country', 'book', 'name', 'word', 'volleyballTeam'] #should be done automatically by searching db_directory
 
 debug				= not True
-project_dir			= ['/root/guessit', '~/guessit'][1]
+project_dir			= ['/root/guessit', '/home/flc/guessit'][1]
 language			= 'fa'
 use_mongo			= False
 
