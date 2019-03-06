54605c0c123cff6cc2c45f840f1723b957a716a4
danial keimasi <danialkeimasi@gmail.com>
2019-03-06 23:36:43 +0330

    Update DataManager.py

--- template_engine/DataManager.py
+++ template_engine/DataManager.py
@@ -1,21 +1,63 @@
+import random
+
+def randChoose(li, number):
+	randli = li[:]
+	random.shuffle(randli)
+	return randli[:number]
+
+
 class DataManager():
-	"""
+	'''
 	Data Manager class for working with datasets and accessing it's data easier
-	
-	"""
+	'''
+	exp = []
 
 	def __init__(self, exp=None):
+		'''
+		constract a DataManager
+		
+		'''
+		self.exp = exp
+
 		if exp:
 			if not isinstance(exp, list):
 				exp = [exp]
-
+			
+			# we want to find keys of
+			# inner list by this for
 			for k in exp[0]:
 				if len(exp) == 1:
 					setattr(self, k, exp[0][k])
 				else:
 					setattr(self, k, [exp[i][k] for i in range(len(exp))])
 
-			if len(exp) > 1:
-				setattr(self, 'list', [DataManager([item]) for item in exp])
-		else:
-			pass
+	def one(self):
+		'''
+		choose random from a list of DataManagers by len of 1
+		
+		---
+			work same as choose(1)
+		
+		'''
+		return randChoose(self.dmlist, 1)
+	
+	def choose(self, num):
+		'''
+		choose random from a list of DataManagers by len of "num"
+		'''
+		return DataManager(randChoose(self.pylist, num))
+
+	@property
+	def pylist(self):
+		'''
+		converts the DataManager object to a python List
+		'''
+		return self.exp
+	
+	@property
+	def dmlist(self):
+		'''
+		converts the DataManager object to a dataManager List
+		'''
+		return [DataManager([item]) for item in self.exp]
+
