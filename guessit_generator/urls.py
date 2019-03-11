8cb72965f4123565bc0cdd58821ca2045371e478
danial keimasi <danialkeimasi@gmail.com>
2019-03-11 19:06:41 +0330

    remove guessit_generator directory

--- guessit_generator/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('generator/', include('generator.urls')),
-    path('admin/', admin.site.urls),
-]
