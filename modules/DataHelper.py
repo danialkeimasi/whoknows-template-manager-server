9eea43720554c18e80930ed2a79df9bcf7df3dcf
onlie6731 <mohammad.parsian@gmail.com>
2019-03-20 19:12:47 +0100

    change in imports

--- modules/DataHelper.py
+++ modules/DataHelper.py
@@ -1,5 +1,5 @@
 import random
-from template_engine.tools import choose
+from tools import choose
 
 class DataHelper():
 	'''
--- modules/TemplateEngine.py
+++ modules/TemplateEngine.py
@@ -12,13 +12,13 @@ import argparse
 import itertools
 from difflib import SequenceMatcher
 import functools
-from template_engine.tools import choose, rand, to_list
+from tools import choose, rand, to_list
 
-from template_engine.checkTemplates import *
-from template_engine.DataHelper import *
-from template_engine.exceptions import *
-from template_engine.checkTags import *
-from template_engine.config import *
+from checkTemplates import *
+from DataHelper import *
+from exceptions import *
+from checkTags import *
+from config import *
 
 
 
