fbc8c9e8e6764e40339f627c5e2f2d5d4a420227
danial keimasi <danialkeimasi@gmail.com>
2019-05-04 09:14:16 +0430

    add the format checker to the template

---
+++ config/config.py
@@ -0,0 +1,51 @@
+import logging
+# from pymongo import MongoClient
+
+
+class CONFIG:
+    """
+    Configurations and settings of project
+
+    TODO : should use a config file to load it
+    """
+
+    debug = not True
+    project_dir = '.'
+    templates_dir = f'{project_dir}/templates/main'
+    questions_dir = f'{project_dir}/modules'
+    result_dir = f'{project_dir}/template_engine/result'
+    dataset_dir = f'{project_dir}/_datasets'
+    language = 'fa'
+    use_mongo = False
+
+    main_server_ip = 'https://31.184.132.183:30000'
+    template_new = f'{main_server_ip}/template/new'
+
+
+datasets = ['movie', 'director', 'song', 'actor', 'footballPlayer', 'footballTeam', 'quote',
+            'country', 'book', 'name', 'word',
+            'volleyballTeam']  # should be done automatically by searching db_directory
+
+logging.basicConfig(
+    datefmt='%y-%b-%d %H:%M:%S',
+    format='%(levelname)8s:[%(asctime)s][%(filename)20s:%(lineno)4s -%(funcName)20s() ]: %(message)s',
+
+    # datefmt='%H:%M:%S',
+    level=logging.CRITICAL,
+    handlers=[
+        # logging.FileHandler(f'{CONFIG.project_dir}/template_engine.log', mode='w+', encoding='utf8', delay=0),
+        logging.StreamHandler(),
+    ]
+)
+
+logger = logging.getLogger('TemplateEngine')
+
+def loggerHandle(loggerFunction, message, problemsList = None):
+    if problemsList != None:
+        problemsList.append(message)
+    
+    loggerFunction(message)
+
+
+# if CONFIG.use_mongo:
+#     mongo = MongoClient('mongodb://localhost:27017')
---
+++ modules/template.py
@@ -0,0 +1,59 @@
+from config.config import logger
+import json
+from pprint import pprint
+
+
+class Template:
+
+    def __init__(self, template_dict):
+        self.template = template_dict
+        pass
+
+    def check_format(self, problems = None):
+        problems = [] if problems is None else problems
+
+        template_consts = ['__level', '__usage', '__values', '__time',
+                           '__score']
+        for item in template_consts:
+            if not (item in self.template):
+                problems.append(f"template must have a '{item}' part in it")
+
+        question_types = [key for key in self.template.keys() if not key.startswith('__')]
+        logger.critical(f'found this question types: {question_types}')
+
+        template_formatter = json.load(open('templates\\template_v2\\template_formatter.json'))
+
+        for q_type in question_types:
+            if not (q_type in template_formatter):
+                problems.append(f'there is an undefined question type in template: {q_type}')
+
+            for q_property_name in self.template[q_type].keys():
+                if not (q_property_name in template_formatter[q_type]):
+                    problems.append(f'there is an undefined part in "{q_type}" in template: {q_property_name}')
+
+                q_property = self.template[q_type][q_property_name]
+                if not (
+                        'format' in q_property and
+                        'content' in q_property and
+                        isinstance(q_property['format'], str) and
+                        isinstance(q_property['content'], list)
+                ):
+                    problems.append(f"wrong syntax for {q_property_name} part in {q_type} question type")
+
+            q_requirements = [item for item in
+                              set(template_formatter[q_type].keys()) - set(self.template[q_type].keys()) if
+                              template_formatter[q_type][item]]
+            if q_requirements:
+                problems.append(f"there is no {q_requirements} in {q_type} type question")
+
+        pprint(problems)
+        return problems
+
+
+    def find_tags(self, problems = None):
+        problems = [] if problems is None else problems
+
+
+    def
+
+
