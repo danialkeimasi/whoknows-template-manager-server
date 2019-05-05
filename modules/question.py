dff9e4642ace5b1b44dbeec0318951daeb0a90b9
danial keimasi <danialkeimasi@gmail.com>
2019-05-05 20:24:21 +0430

    Update question.py

---
+++ modules/question.py
@@ -0,0 +1,24 @@
+
+
+class Question:
+
+    def __init__(self, question_dict = None):
+        self.question = question_dict
+
+
+    def update_by_dict(self, question_dict):
+        self.question = question_dict
+
+
+    def get_dict(self):
+        return self.question
+
+
+    def check_question(self):
+        problems = []
+
+        return problems
+
+
+
+
