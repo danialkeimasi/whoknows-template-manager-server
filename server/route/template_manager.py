147a920b7351527633e0092688ccedd14f6ed8c1
danial keimasi <danialkeimasi@gmail.com>
2019-05-10 03:00:53 +0430

    add template_manager html file and route

---
+++ server/route/template_manager.py
@@ -0,0 +1,12 @@
+from server.flask import getApp
+from flask import json, request, render_template
+
+
+
+
+def add():
+    app = getApp()
+    @app.route('/template_manager', methods=['GET'])
+    def template_manager():
+
+        return render_template('_pages/template_manager/index.html')
--- server/routeHandle.py
+++ server/routeHandle.py
@@ -1,8 +1,8 @@
-from server.route import index, get_question, check_answer, test_template
+from server.route import index, get_question, check_answer, test_template, template_manager
 
 def addRoutes():
     index.add()
     get_question.add()
     check_answer.add()
     test_template.add()
-    
+    template_manager.add()
