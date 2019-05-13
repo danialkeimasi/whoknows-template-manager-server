f9c893bc3ba04f241e4e7aca9878eff6e747b83b
danial keimasi <danialkeimasi@gmail.com>
2019-05-13 18:20:15 +0430

    add the mongo log handler, add the mongo client, delete old CONFIG class and add yaml file

--- config/config.py
+++ config/config.py
@@ -2,35 +2,13 @@ import logging
 import pyyaml
 import attrdict
 from pymongo import MongoClient
+from log4mongo.handlers import MongoHandler
 from pprint import pprint
+
 
 with open("./config/config.yml" , 'r') as yamlfileobj:
     config = attrdict.AttrDict(yaml.safe_load(yamlfileobj))
 
-
-class CONFIG:
-    """
-    Configurations and settings of project
-
-    TODO : should use a config file to load it
-    """
-
-    debug = not True
-    project_dir = '.'
-    templates_dir = f'{project_dir}/templates/main'
-    questions_dir = f'{project_dir}/modules'
-    result_dir = f'{project_dir}/template_engine/result'
-    dataset_dir = f'{project_dir}/_datasets'
-    language = 'fa'
-    use_mongo = False
-
-    main_server_ip = 'https://31.184.132.183:30000'
-    template_new = f'{main_server_ip}/template/new'
-
-
-datasets = ['movie', 'director', 'song', 'actor', 'footballPlayer', 'footballTeam', 'quote',
-            'country', 'book', 'name', 'word',
-            'volleyballTeam']  # should bea done automatically by searching db_directory
 
 logging.basicConfig(
     datefmt='%y-%b-%d %H:%M:%S',
@@ -39,15 +17,23 @@ logging.basicConfig(
     # datefmt='%H:%M:%S',
     level=logging.DEBUG,
     handlers=[
-        # logging.FileHandler(f'{CONFIG.project_dir}/template_engine.log', mode='w+', encoding='utf8', delay=0),
+        # logging.FileHandler(f'{config.dir.project}/template_engine.log', mode='w+', encoding='utf8', delay=0),
         logging.StreamHandler(),
+        MongoHandler(host=config.mongo.ip, port=config.mongo.port,
+                     username=config.mongo.username, password=config.mongo.password,
+                     authentication_db=config.mongo.authentication_db, database_name='TemplateManager', collection='log'),
+
     ]
 )
-
 logger = logging.getLogger('TemplateEngine')
 
+mongo_client = MongoClient(
+    f'mongodb://{config.mongo.username}:{config.mongo.password}@{config.mongo.ip}:{config.mongo.port}'
+    f'/?authSource={config.mongo.authentication_db}')
+
+
 def loggerHandle(loggerFunction, message, problemsList = None):
-    if problemsList != None:
+    if problemsList is not None:
         problemsList.append(message)
     
     loggerFunction(message)
