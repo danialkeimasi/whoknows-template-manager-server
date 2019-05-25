9e260d6280c683202e790ddaf9742ac7ffabcab5
danial keimasi <danialkeimasi@gmail.com>
2019-05-25 19:23:45 +0430

    Update find_template.py

--- server/route/find_template.py
+++ server/route/find_template.py
@@ -29,11 +29,12 @@ def add():
             find template py post request
         '''
         user_req = request.json if request.json is not None else {}
+        query    = user_req['query']    if 'query' in user_req else None
         tags     = user_req['tags']     if 'tags'  in user_req else None
         ok       = user_req['ok']       if 'ok'    in user_req else None
         count    = user_req['count']    if 'count' in user_req else None
     
-        pipeline = \
+        pipeline = [{'$match' : query }]                       if query is not None else [] + \
                    [{'$match' : {'ok': ok} }]                  if ok    is not None else [] + \
                    [{'$match' : {'tags': { '$in' : tags } } }] if tags  is not None else [] + \
                    [{'$limit': count}]                         if count is not None else []
