1bdfccfa9662775591edb5ae62d1d8f19d614d65
danial keimasi <danialkeimasi@gmail.com>
2019-06-04 14:56:05 +0430

    Update question_generate.py

--- api/routes/question_generate.py
+++ api/routes/question_generate.py
@@ -32,7 +32,7 @@ def add():
         tags = user_req['tags'] if 'tags' in user_req else None
         count = user_req['count'] if 'count' in user_req else 1
 
-        if tags is None or count is None:
+        if tags is None:
             response = {
                 "ok": False
             }
