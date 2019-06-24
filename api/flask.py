c17d3716f1e9fbd76fc68e5b492bb09af1b4bfc0
danial keimasi <danialkeimasi@gmail.com>
2019-06-24 20:56:13 +0430

    merge flask.py with routesHandler file Fix #39

--- api/flask.py
+++ api/flask.py
@@ -1,6 +1,26 @@
+import glob
+import importlib
+import os
+
+
+def add_routes():
+    """ add the routes to the flask app. """
+
+    files = [os.path.basename(path)[:-3] for path in glob.glob('./api/routes/*.py')
+                                            if not os.path.basename(path).startswith('__')]
+
+    for file in files:
+        importlib.import_module(f'api.routes.{file}').add()
+
+
 def getApp():
     return app
+
+def getApi():
+    return api_app
+
 
 
 if __name__ == "__main__":
     app = None
+    api_app = None
--- app.py
+++ app.py
@@ -1,17 +1,15 @@
+import api.flask as flaskApp
+
 from flask import Flask
-
 from flask_cors import CORS
-import api.routesHandler
-import api.flask
-
 from config.arg_parse import arg_parse
 
-api.flask.app = Flask(__name__)
-CORS(api.flask.getApp())
-api.routesHandler.add_routes()
-
 if __name__ == '__main__':
+    flaskApp.app = Flask(__name__)
+    CORS(flaskApp.getApp())
+    flaskApp.add_routes()
+
     isThereArg = arg_parse()
 
     if not isThereArg:
-        api.flask.getApp().run(debug=True, host='0.0.0.0', port='3001')
+        flaskApp.getApp().run(debug=True, host='0.0.0.0', port='3001')
