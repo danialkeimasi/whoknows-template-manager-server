8cb8cd93d1dab74ff62e888a41b241c57993658f
danial keimasi <danialkeimasi@gmail.com>
2019-06-24 21:04:52 +0430

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     isThereArg = arg_parse()
 
     if not isThereArg:
-        flaskApp.getApp().run(debug=True, port='3001')
+        flaskApp.getApp().run(debug=True, host='0.0.0.0', port='3001')
