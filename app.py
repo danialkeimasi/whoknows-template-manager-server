4d8ed3b86f02095ddbb57309871b28b8d5638115
danial keimasi <danialkeimasi@gmail.com>
2019-06-26 19:17:23 +0430

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 from importlib import import_module
-from glob import glob
 from os.path import basename
+from glob import glob
 
 from config.arg_parse import arg_parse
-from flask import Flask
 from flask_cors import CORS
+from flask import Flask
 
 if __name__ == '__main__':
     app = Flask(__name__)
     CORS(app)
 
-    for file in [basename(path).replace('.py', '') for path in glob('./routes/*.py') if not basename(path).startswith('__')]:
+    for file in [basename(path).split('.')[0] for path in glob('./routes/*.py') if not basename(path).startswith('__')]:
         import_module(f'routes.{file}').add(app)
 
     isThereArg = arg_parse()
 
     if not isThereArg:
         app.run(debug=True, host='0.0.0.0', port='3001')
-
