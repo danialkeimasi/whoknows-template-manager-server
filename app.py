2b2cbd85eb75085a127f5dbe2ec5ff18f6b55f9c
danial keimasi <danialkeimasi@gmail.com>
2019-07-06 14:18:36 +0430

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask_cors import CORS
 from flask import Flask
 
 if __name__ == '__main__':
+
     app = Flask(__name__)
     CORS(app)
 
@@ -16,4 +17,4 @@ if __name__ == '__main__':
     isThereArg = arg_parse()
 
     if not isThereArg:
-        app.run(debug=True, host='0.0.0.0' port='3001')
+        app.run(debug=True, host='0.0.0.0', port='3001')
