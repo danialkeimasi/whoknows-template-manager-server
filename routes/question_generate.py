82a986edaa5732516e4a7591d5a55c632955258e
danial keimasi <danialkeimasi@gmail.com>
2019-07-23 10:29:49 +0430

    update the route to get the metadata

--- routes/question_generate.py
+++ routes/question_generate.py
@@ -11,19 +11,24 @@ from modules.tools.functions import traceback_shortener
 questions_sample = json.load(open(config.dir.sample_questions, encoding='utf-8'))
 
 
-
 parser = flask_restplus.reqparse.RequestParser()
 parser.add_argument(
     'tags',
     type=list,
-    help='you must send the "idea" of template as a post json request.',
+    help='you must send the "tags" of template as a post json request.',
     required=True
 )
 parser.add_argument(
     'count',
     type=int,
-    help='you must send the "idea" of template as a post json request.',
+    help='you must send the "count" of template as a post json request.',
     required=True
+)
+parser.add_argument(
+    'metadata',
+    type=dict,
+    help='you must send the "metadata" of template as a post json request.',
+    required=False
 )
 
 
@@ -39,44 +44,38 @@ def add(api):
 
         def post(self):
 
-            user_req = request.json
-            tags = user_req['tags'] if 'tags' in user_req else None
-            count = user_req['count'] if 'count' in user_req else 1
+            args = parser.parse_args()
 
-            if tags is None:
-                response = {
-                    "ok": False
-                }
-            else:
+            tags = args['tags']
+            count = args['count']
+            metadata = args['metadata']
 
-                templates = list(mongo_client.template_manager.templates.aggregate([
-                    {'$match': {'__state': 'in_use'}},
-                    {'$match': {'tags': {'$in': tags}}},
-                    {'$sample': {'size': count}},
-                ]))
-                logger.debug(f'after query for template, number of templates that we found: {len(templates)}')
+            templates = list(mongo_client.template_manager.templates.aggregate([
+                {'$match': {'__state': 'in_use'}},
+                {'$match': {'tags': {'$in': tags}}},
+                {'$sample': {'size': count}},
+            ]))
 
-                questions = []
+            logger.debug(f'after query for template, number of templates that we found: {len(templates)}')
 
-                try_count = 5
-                i = 0
-                while (len(questions) < count) and (i < count * try_count):
-                    for _ in range(try_count):
-                        try:
-                            questions.append(Template(templates[i % len(templates)]).generate_question().raise_if_problems().dict())
-                            break
-                        except Exception as e:
-                            error_message = traceback_shortener(traceback.format_exc())
-                            logger.error(f'failed in generate question => {error_message}')
-                    else:
-                        # TODO: do some report for this template to the database
-                        pass
+            questions = []
+            try_count = 5
+            i = 0
+            while (len(questions) < count) and (i < count * try_count):
+                for _ in range(try_count):
+                    try:
+                        questions.append(Template(templates[i % len(templates)]).generate_question().raise_if_problems().dict())
+                        break
+                    except Exception as e:
+                        error_message = traceback_shortener(traceback.format_exc())
+                        logger.error(f'failed in generate question => {error_message}')
+                else:
+                    # TODO: do some report for this template to the database
+                    pass
 
-                    i += 1
+                i += 1
 
-                response = {
-                    'ok': len(questions) == count,
-                    'questions': questions,
-                }
-
-            return json_tools.to_extended(response)
+            return json_tools.to_extended({
+                'ok': len(questions) == count,
+                'questions': questions,
+            })
