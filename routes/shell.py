d8ad62c694ccaa27bb2cad70c60cfcc5381105c5
danial keimasi <danialkeimasi@gmail.com>
2019-08-05 16:06:01 +0430

    adding shell route

---
+++ routes/shell.py
@@ -0,0 +1,36 @@
+import io
+import subprocess
+
+import flask_restplus
+from bson import json_util
+from flask import json, request
+
+from config.config import config, mongo_client
+from modules.tools import json_tools
+
+parser = flask_restplus.reqparse.RequestParser()
+parser.add_argument(
+    'command',
+    type=str,
+    help='you must send the "command" as a post json request.',
+    required=True
+)
+
+
+def add(api):
+    @api.route('/shell')
+    class ShellRoute(flask_restplus.Resource):
+        """
+        """
+
+        def post(self):
+
+            args = parser.parse_args()
+            command = args['command'].split()
+
+            proc = subprocess.Popen(["python3", "app.py"] + command, stdout=subprocess.PIPE)
+            response_list = [line for line in io.TextIOWrapper(proc.stdout, encoding="utf-8")]
+
+            return json_tools.to_extended({
+                'response': response_list
+            })
