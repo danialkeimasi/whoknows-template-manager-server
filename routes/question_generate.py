a5ebd171d9c0f236394424e57dd45809fb79a33d
danial keimasi <danialkeimasi@gmail.com>
2019-08-06 21:30:37 +0430

    generate question - debugging

--- routes/question_generate.py
+++ routes/question_generate.py
@@ -64,7 +64,7 @@ def add(api):
             count = args['count']
             metadata = args['metadata']
 
-            pipeline = [{'$match': {'tags': {'$in': tags}}}]
+            pipeline = [{'$match': {'__state': 'in_use'}}]
             pipeline += [{'$match': {'tags': {'$in': tags}}}] if tags is not None else []
             pipeline += [{'$sample': {'size': count}}] if count is not None else []
             pipeline += [{'$match': query}] if query is not None else []
