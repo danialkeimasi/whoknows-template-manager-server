b77804d5ccac5fa058fb6602d8edbfd9e17b9f2e
Danial Keimasi <danialkeimasi@gmail.com>
2019-08-16 21:49:16 +0430

    update dataset_load route. now the only arg is query.

--- routes/dataset_load.py
+++ routes/dataset_load.py
@@ -1,22 +1,17 @@
 import os
 
+from os.path import basename
+from glob import glob
+
 import flask_restplus
 
 from flask import json, request
 
-from config.config import mongo_client
+from config.config import mongo_client, config
 
 from modules.tools import json_tools
 
 parser = flask_restplus.reqparse.RequestParser()
-parser.add_argument(
-    'dataset_names',
-    type=list,
-    location='json',
-    help='query with names',
-    required=False,
-    default=[]
-)
 parser.add_argument(
     'query',
     type=dict,
@@ -37,26 +32,22 @@ def add(api):
         """
 
         def post(self):
-            dataset_dir = '../datasets/'
-
             args = parser.parse_args()
             dataset_query = args['query']
-            dataset_names = args['dataset_names']
 
-            if dataset_names == []:
-                dataset_names += list(map(lambda i: i['headers']['name'],
-                                    mongo_client.data_manager.datasets.find(dataset_query)))
+            dataset_names = list(map(lambda i: i['headers']['name'],
+                                mongo_client.data_manager.datasets.find(dataset_query)))
 
             response_list = []
-            for dataset_name in set(dataset_names):
+            for dataset_name in dataset_names:
                 dataset = list(mongo_client.data[dataset_name].find())
                 if len(dataset) != 0:
-                    if not os.path.isdir(f'{dataset_dir}'):
-                        os.mkdir(f'{dataset_dir}')
+                    if not os.path.isdir(f'{config.dir.dataset}'):
+                        os.mkdir(f'{config.dir.dataset}')
 
                     json.dump(
                         json_tools.to_extended(dataset),
-                        open(f'{dataset_dir}{dataset_name}db.json', mode='w+', encoding='utf-8'),
+                        open(f'{config.dir.dataset}/{dataset_name}db.json', mode='w+', encoding='utf-8'),
                         indent=4, ensure_ascii=False
                     )
 
@@ -66,7 +57,17 @@ def add(api):
                     'ok': len(dataset) != 0,
                 })
 
+            datasets_in_data_manager = list(map(lambda i: i['headers']['name'],
+                                            mongo_client.data_manager.datasets.find(dataset_query)))
+
+            for file in [basename(path).split('.')[0] for path in glob(f'{config.dir.dataset}/*')]:
+                if file[:-2] not in datasets_in_data_manager:
+                    os.remove(f'{config.dir.dataset}/{file}.json')
+
             return {
                 'ok': all([i['ok'] for i in response_list]),
-                'datasets': response_list
+                'updated_datasets': response_list,
+                'problems': [
+                    f"there is some problem with '{dataset['name']}' dataset" for dataset in response_list if not dataset['ok']
+                ],
             }
