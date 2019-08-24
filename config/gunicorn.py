6637bd837c46beeeb2450b6f005d80448f255f70
Danial Keimasi <danialkeimasi@gmail.com>
2019-08-24 20:29:16 +0430

    add gunicorn config file

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -183,17 +183,22 @@ proc_name = None
 #       A callable that takes a server instance as the sole argument.
 #
 
+
 def post_fork(server, worker):
     server.log.info("Worker spawned (pid: %s)", worker.pid)
+
 
 def pre_fork(server, worker):
     pass
 
+
 def pre_exec(server):
     server.log.info("Forked child, re-executing.")
 
+
 def when_ready(server):
     server.log.info("Server is ready. Spawning workers")
+
 
 def worker_int(worker):
     worker.log.info("worker received INT or QUIT signal")
