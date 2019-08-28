ca4e2d6f5a1b3eb074305e76b9b2a1af6d1d62a5
Danial Keimasi <danialkeimasi@gmail.com>
2019-08-28 14:24:47 +0430

    add tests/ directory

--- config/command_line_interface.py
+++ config/command_line_interface.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 import click
+import unittest
 
 from pprint import pprint
 from modules.template import Template
@@ -67,3 +68,11 @@ def run(command, template):
     template = mongo_client.template_manager.templates.find_one({'_id': ObjectId(template)})
     response = Template(template).parse(run_command=command)
     pprint(response)
+
+
+@cli.command()
+def runtest():
+    print('ppppooooo')
+    for file in [basename(path).split('.')[0] for path in glob('./test/*.py') if not basename(path).startswith('__')]:
+        import_module(f'test.{file}').main()
+
