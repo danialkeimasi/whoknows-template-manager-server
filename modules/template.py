108022fd4e490c0d553ec98a3e58ee2e8e7475b1
Danial Keimasi <danialkeimasi@gmail.com>
2019-08-30 12:19:28 +0430

    debugging

--- modules/template.py
+++ modules/template.py
@@ -204,8 +204,8 @@ class Template:
         question_type = choose(self.get_question_types(), 0) if not question_type else \
             f'{config.format.question.exist}{question_type}'
 
-        question_object = self.parse(metadata=metadata, bool_answer=bool_answer)
-                              .get_question(bool_answer=bool_answer, question_type, question_format)
+        question_object = self.parse(metadata=metadata, bool_answer=bool_answer) \
+                              .get_question(bool_answer, question_type, question_format)
 
         return question_object
 
