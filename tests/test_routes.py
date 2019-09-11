1671cc4e74d598091c35c2bdbb3dcad371ff3cdb
Danial Keimasi <danialkeimasi@gmail.com>
2019-09-11 23:34:19 +0430

    working on tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -3,6 +3,8 @@ from flask import json
 from config import mongo_client
 from app import app
 
+from modules.tools import json_tools
+
 
 def test_dataset_load():
     response = app.test_client().post(
@@ -28,16 +30,22 @@ def test_question_generate():
     assert response_data['ok']
 
 
-# def test_shell():
-#     response = app.test_client().post(
-#         '/shell',
-#         json={'command': 'test'},
-#     )
 
-#     response_data = json.loads(response.get_data(as_text=True))
+def test_shell():
+    template_id = json_tools.to_extended(list(mongo_client.template_manager.templates.aggregate([
+                {'$match': {'__state': 'in_use'}},
+                {'$sample': {'size': 1}}
+            ]))[0]['_id'])['$oid']
 
-#     assert 'response' in response_data
-#     assert response_data['response'] != []
+    response = app.test_client().post(
+        '/shell',
+        json={'command': f'test -t {template_id}'},
+    )
+    response_data = json.loads(response.get_data(as_text=True))
+    print(response_data)
+    assert 'response' in response_data
+    assert response_data['response'] != []
+
 
 
 # def test_template_test():
