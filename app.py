ee3466e5ea7fb6a023b5ffaee19c59b9cdf9bbf0
Danial Keimasi <danialkeimasi@gmail.com>
2019-09-21 21:22:34 +0430

    using settings yaml file for get datasets/ in app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import os
 
+from config import SETTINGS
 from modules.command_line_interface import cli, app
 
-if not os.path.isdir('datasets/'):
+
+if not os.path.isdir(SETTINGS.dir.dataset):
     app.test_client().post('/dataset/load', json={})
+
 
 if __name__ == "__main__":
     cli()
