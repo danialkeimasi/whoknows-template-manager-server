80154a4d6fd325ca5c939f3dc49efcb4a54ad9ff
Danial Keimasi <danialkeimasi@gmail.com>
2019-09-21 19:35:13 +0430

    add if statement for dataset downloader

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import os
+
 from modules.command_line_interface import cli, app
 
-app.test_client().post('/dataset/load', json={})
+if not os.path.isdir('datasets/'):
+    app.test_client().post('/dataset/load', json={})
 
 if __name__ == "__main__":
     cli()
