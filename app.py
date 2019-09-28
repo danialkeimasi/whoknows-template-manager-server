3a707f838c87a00b66a94b9a2277ec66d121f249
Danial Keimasi <danialkeimasi@gmail.com>
2019-09-28 19:53:26 +0330

    data reload schedule.

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import os
+import time
+
+import schedule
 
 from config import SETTINGS
 from modules.command_line_interface import cli, app
@@ -6,6 +9,13 @@ from modules.command_line_interface import cli, app
 
 if not os.path.isdir(SETTINGS.dir.dataset):
     app.test_client().post('/dataset/load', json={})
+
+
+def reload():
+    app.test_client().post('/dataset/load', json={})
+
+
+schedule.every(SETTINGS.reload_period).minutes.do(reload)
 
 
 if __name__ == "__main__":
