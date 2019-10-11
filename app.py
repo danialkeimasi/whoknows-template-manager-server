2d1d28bb149ea877b267e2bdf11988ff8771b8fa
Danial Keimasi <danialkeimasi@gmail.com>
2019-10-11 22:34:13 +0330

    delete data reload thred definition in the app.py

--- app.py
+++ app.py
@@ -1,16 +1,4 @@
-from threading import Thread
-
-import schedule
-import modules.data_reload
-
 from modules.command_line_interface import cli, app
-from config import SETTINGS
-
-
-if SETTINGS.data_reload:
-    schedule.every(SETTINGS.reload_period).minutes.do(modules.data_reload.reload)
-    t = Thread(target=modules.data_reload.run_schedule)
-    t.start()
 
 
 if __name__ == "__main__":
