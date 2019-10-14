60d26efbb527c4fed5bfe4936e19575eee2e477d
Danial Keimasi <danialkeimasi@gmail.com>
2019-10-14 14:38:23 +0330

    new codes commented

--- modules/template.py
+++ modules/template.py
@@ -107,18 +107,20 @@ class Template:
                 continue
 
             for i, raw_str in enumerate(types_list):
-                if raw_str[1] == 
-                while re.search(reg_str, raw_str):
-                    exp = re.search(reg_str, raw_str).group(1)
-                    try:
-                        eval_result = eval(exp)
-                    except Exception as e:
-                        raise type(e)(f"in the validating [{key}][{i}]: `{exp}`: {e}") from e
-                    else:
-                        raw_str = raw_str.replace(f'`{exp}`', eval_result[0] if \
-                            isinstance(eval_result, list) else str(eval_result))
 
-                dotted_question_part[key][i] = raw_str
+
+            #     if raw_str[1] == 
+            #     while re.search(reg_str, raw_str):
+            #         exp = re.search(reg_str, raw_str).group(1)
+            #         try:
+            #             eval_result = eval(exp)
+            #         except Exception as e:
+            #             raise type(e)(f"in the validating [{key}][{i}]: `{exp}`: {e}") from e
+            #         else:
+            #             raw_str = raw_str.replace(f'`{exp}`', eval_result[0] if \
+            #                 isinstance(eval_result, list) else str(eval_result))
+
+            #     dotted_question_part[key][i] = raw_str
 
 
 
